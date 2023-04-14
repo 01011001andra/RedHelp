8510e8946a972774400584b0e87e89d99f6dadbe
budi prayoga <budiprayoga5103@gmail.com>
2023-04-14 15:17:38 +0700

    add Login Form

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#E4011F"
+      },
+    },
+ 
   },
   plugins: [require("daisyui")],
 };
