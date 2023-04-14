d3f0088beefecf326056d900aa6f47b4df835eea
Yandra Muslim <yandrayt1@gmail.com>
2023-04-14 13:41:07 +0700

    ganti hover

--- src/pages/Halaman/Notfound.jsx
+++ src/pages/Halaman/Notfound.jsx
@@ -14,7 +14,7 @@ const Notfound = () => {
         </p>
         <Link
           to={"/"}
-          className="flex items-center space-x-2 bg-[#E4011F] hover:bg-blue-700 text-gray-100 px-4 py-2 mt-12 rounded transition duration-150"
+          className="flex items-center space-x-2 bg-[#E4011F] hover:bg-green-700 text-gray-100 px-4 py-2 mt-12 rounded transition duration-150"
           title="Return Home"
         >
           <svg
