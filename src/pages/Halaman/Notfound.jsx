f490620fe3cd5f0539907ca435ba6178906fc1bd
budi prayoga <budiprayoga5103@gmail.com>
2023-04-14 14:50:24 +0700

    Pages 404

--- src/pages/Halaman/Notfound.jsx
+++ src/pages/Halaman/Notfound.jsx
@@ -3,23 +3,23 @@ import { Link } from "react-router-dom";
 
 const Notfound = () => {
   return (
-    <div class="w-full h-screen flex flex-col items-center justify-center">
-      <h1>FOTO</h1>
+    <div class="w-full h-screen flex flex-col items-center justify-center  bg-white">
+      <img className="w-64 h-64" src="./404.gif" alt="404" />
       <div class="flex flex-col items-center justify-center">
         <p class="text-3xl md:text-4xl lg:text-5xl text-gray-800 mt-12">
           Page Not Found
         </p>
-        <p class="md:text-lg lg:text-xl text-gray-600 mt-8">
-          Sorry, the page you are looking for could not be found.
+        <p className="md:text-lg lg:text-xl text-gray-600 mt-8">
+          Maaf, Halaman yang anda cari tidak dapat ditemukan
         </p>
         <Link
           to={"/"}
-          class="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 mt-12 rounded transition duration-150"
+          className="flex items-center space-x-2 bg-[#E4011F] hover:bg-green-700 text-gray-100 px-4 py-2 mt-12 rounded transition duration-150"
           title="Return Home"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
+            className="h-5 w-5"
             viewBox="0 0 20 20"
             fill="currentColor"
           >
